05d69fdebf62ed9affebde8b96f6ff1d565ec780
GaspardP <gaspardp@kth.se>
2020-07-06 22:29:43 +0200

    [02] Turn off all output processing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate termios;
 
 use nix::unistd::read;
 use std::os::unix::io::RawFd;
-use termios::{tcgetattr, tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, IXON, TCSAFLUSH};
+use termios::{
+    tcgetattr, tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, IXON, OPOST, TCSAFLUSH,
+};
 
 /// The `ECHO` feature prints each key typed in the terminal. This is the
 /// default behaviour in cannonical mode. This function makes sure the feature
@@ -16,6 +18,10 @@ use termios::{tcgetattr, tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, 
 /// deactivate the transformation of all carriage return characters to newlines.
 /// The `IXON` flag is used to deactivate the software control flow control
 /// characters (C-s and C-q). Those were used to pause transmission of input.
+/// The `OPOST` flag is used to turn off the output processing features (such as
+/// moving the cursor back to the begining of the line when the carriage return
+/// '\r' input is received). Because of the cursor needs to be explicitly
+/// "returned" to the begining of the line when calling `print!`.
 ///
 /// Terminal attributes can be read with `tcgetattr` and changed with
 /// `tcsetattr`. `TCSAFLUSH` specifies that the changes will be applied once all
@@ -26,11 +32,13 @@ use termios::{tcgetattr, tcsetattr, Termios, ECHO, ICANON, ICRNL, IEXTEN, ISIG, 
 /// tcgetattr(STDIN_FILENO, &raw);
 /// raw.c_iflag &= ~(ICRNL | IXON);
 /// raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+/// raw.c_oflag &= ~(OPOST);
 /// tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 fn enable_raw_mode(fd: RawFd, mut termios: Termios) -> Result<(), std::io::Error> {
     tcgetattr(fd, &mut termios)?;
     termios.c_iflag &= !(ICRNL | IXON);
     termios.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+    termios.c_oflag &= !(OPOST);
     tcsetattr(fd, TCSAFLUSH, &termios)?;
     // Returns Result::Ok if none of the previous function calls triggered an
     // error. Errors will get automatically propagated thanks to the `?` try
@@ -53,12 +61,12 @@ fn main() {
     let original_termios = if let Result::Ok(termios) = Termios::from_fd(stdin) {
         termios
     } else {
-        println!("Could not create termios instance");
+        print!("Could not create termios instance\r\n");
         return;
     };
 
     if let Result::Err(e) = enable_raw_mode(stdin, original_termios) {
-        println!("Could not activate raw mode: {:?}", e);
+        print!("Could not activate raw mode: {:?}\r\n", e);
         return;
     }
 
@@ -73,23 +81,26 @@ fn main() {
             let ch = c[0] as char;
 
             if ch.is_control() {
-                println!("number of bytes read {:?}: {:?}", bytes_read, c);
+                print!("number of bytes read {:?}: {:?}\r\n", bytes_read, c);
             } else {
-                println!("number of bytes read {:?}: {:?} ('{}')", bytes_read, c, ch);
+                print!(
+                    "number of bytes read {:?}: {:?} ('{}')\r\n",
+                    bytes_read, c, ch
+                );
             }
 
             if 0 == bytes_read || b'q' == c[0] {
-                println!("no more input, exiting");
+                print!("no more input, exiting\r\n");
                 // breaking out of the loop instead of returning to make sure we
                 // run the clean up functions.
                 break;
             }
         } else {
-            println!("Error while reading stdin");
+            print!("Error while reading stdin\r\n");
         }
     }
 
     if let Result::Err(e) = disable_raw_mode(stdin, original_termios) {
-        println!("Could not deactivate raw mode: {:?}", e);
+        print!("Could not deactivate raw mode: {:?}\r\n", e);
     }
 }
