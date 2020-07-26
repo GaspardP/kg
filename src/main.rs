436195560f785c10c4fd501e7d4115c4e43b40b0
GaspardP <gaspardp@kth.se>
2020-07-26 10:26:36 +0200

    [03] The last line
    Differing a bit from the C implementation as I find it more obvious that the
    last line is different from the rest.

--- src/main.rs
+++ src/main.rs
@@ -299,13 +299,17 @@ fn get_window_size(stdin: RawFd, stdout: RawFd) -> Result<(u16, u16), Error> {
 /// ---
 /// int y;
 /// for (y = 0; y < E.screenrows; y++) {
-///   write(STDOUT_FILENO, "~\r\n", 3);
+///    write(STDOUT_FILENO, "~", 1);
+///    if (y < E.screenrows - 1) {
+///      write(STDOUT_FILENO, "\r\n", 2);
+///    }
 /// }
 fn editor_draw_rows(editor_config: &EditorConfig) -> Result<(), Error> {
     let stdout = editor_config.stdout;
-    for _ in 0..editor_config.screen_rows {
+    for _ in 0..(editor_config.screen_rows - 1) {
         write(stdout, b"~\r\n")?;
     }
+    write(stdout, b"~")?;
     Result::Ok(())
 }
 
