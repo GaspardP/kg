1f8ada4941f5304ad4e6d4ff976db5f5d87d8c1b
GaspardP <gaspardp@kth.se>
2020-08-07 18:54:57 +0200

    [04] Horizontal scrolling - part 4
    Make sure the cursor cannot go further than the end of the line.
    
    Given the earlier choice of handling a number of "times" for the cursor to move
    in the `editor_move_cursor` function, we need to handle cases where `times > 1`.
    This makes the change a bit more verbose than the C version.

--- src/main.rs
+++ src/main.rs
@@ -589,6 +589,7 @@ fn editor_refresh_screen(editor_config: &EditorConfig) -> Result<(), Error> {
 /// Using "saturating" addition and substraction to have controlled overflow.
 /// Thanks to that the min check can be skipped for substractions.
 /// ---
+/// erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 /// switch (key) {
 ///   case ARROW_LEFT:
 ///     if (E.cx != 0) {
@@ -596,7 +597,9 @@ fn editor_refresh_screen(editor_config: &EditorConfig) -> Result<(), Error> {
 ///     }
 ///     break;
 ///   case ARROW_RIGHT:
+///     if (row && E.cx < row->size) {
 ///       E.cx++;
+///     }
 ///     break;
 ///   case ARROW_UP:
 ///     if (E.cy != 0) {
@@ -604,7 +607,7 @@ fn editor_refresh_screen(editor_config: &EditorConfig) -> Result<(), Error> {
 ///     }
 ///     break;
 ///   case ARROW_DOWN:
-///     if (E.cy != E.screenrows - 1) {
+///     if (E.cy < E.numrows) {
 ///       E.cy++;
 ///     }
 ///     break;
@@ -613,16 +616,22 @@ fn editor_move_cursor(editor_config: &mut EditorConfig, direction: &Direction, t
     use std::convert::TryFrom;
     use Direction::{Down, Left, Right, Up};
 
-    let (cx, cy) = editor_config.cursor;
-    let max_cx = cx.saturating_add(times);
-    let max_cy = cy.saturating_add(times);
-    let max_y = u16::try_from(editor_config.rows.len()).unwrap_or(u16::MAX);
+    let (current_cx, current_cy) = editor_config.cursor;
+    let expected_cx = current_cx.saturating_add(times);
+    let expected_cy = current_cy.saturating_add(times);
+    let line_length = editor_config
+        .rows
+        .get(current_cy as usize)
+        .map_or(0, String::len);
+    let number_lines = editor_config.rows.len();
+    let max_x = u16::try_from(line_length).unwrap_or(u16::MAX);
+    let max_y = u16::try_from(number_lines).unwrap_or(u16::MAX);
 
     let cursor = match direction {
-        Down => (cx, min(max_y, max_cy)),
-        Up => (cx, cy.saturating_sub(times)),
-        Right => (max_cx, cy),
-        Left => (cx.saturating_sub(times), cy),
+        Down => (current_cx, min(max_y, expected_cy)),
+        Up => (current_cx, current_cy.saturating_sub(times)),
+        Right => (min(max_x, expected_cx), current_cy),
+        Left => (current_cx.saturating_sub(times), current_cy),
     };
 
     editor_config.cursor = cursor;
