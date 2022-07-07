eeb41e9d66d17d49ed19689ff816a7c6f0353c29
GaspardP <gaspardp@kth.se>
2022-07-07 09:46:24 +0200

    Implement better parsing solution - part 1
    Adds tree-sitter so we can move away from the old "manual" parsing and use a
    production grade solution that handles more languages and edge-cases.

---
+++ src/syntax.rs
@@ -0,0 +1,22 @@
+use tree_sitter::{InputEdit, Parser, Point, Tree};
+
+pub fn c() -> Parser {
+    let mut parser = Parser::new();
+    parser
+        .set_language(tree_sitter_c::language())
+        .expect("Error loading C grammar");
+    parser
+}
+
+pub fn rust() -> Parser {
+    let mut parser = Parser::new();
+    parser
+        .set_language(tree_sitter_rust::language())
+        .expect("Error loading rust grammar");
+    parser
+}
+
+pub fn load_code(parser: Option<&mut Parser>, rows: &[String]) -> Option<Tree> {
+    // Parsing
+    parser.and_then(|p| p.parse(rows.join("\n").as_bytes(), None))
+}
